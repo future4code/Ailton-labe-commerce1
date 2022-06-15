67f440f52d064598193e009b8ec38f6ab96cb9e2
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-06-15 15:37:44 -0300

    Componentes carrinho e filtro

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Filtro from "./Componentes/Filtro";
+import Carrinho from "./Componentes/Carrinho";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filtro></Filtro>
+
+
+      <Carrinho></Carrinho>
     </div>
   );
 }
