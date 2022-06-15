0d8f4c6a7e3ec7a78f3913a4d97106c222561d2d
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2022-06-15 16:24:06 -0300

    pagina produtos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Filtro from "./Componentes/Filtro";
 import Carrinho from "./Componentes/Carrinho";
+import PaginaProdutos from "./Componentes/PaginaProdutos"
 import './App.css';
 
 function App() {
